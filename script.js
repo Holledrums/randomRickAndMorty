400404f4ab4470b0fc8561591fabdee30e3aea40
Holger <holgeraliusklose@gmail.com>
2022-06-20 14:38:01 +0200

    solution

--- script.js
+++ script.js
@@ -11,19 +11,21 @@ async function rickMortyCharacters() {
 }
 
 function randomCharacters(data) {
-  const imageRandom = Math.floor(Math.random() * data.results.length);
-  const character = data.results[imageRandom];
-  console.log(character);
-  const img = document.createElement("img");
-  const name = document.createElement("p");
-  const race = document.createElement("p");
-  const episode = document.createElement("a");
-  img.src = character.image;
-  name.textContent = `Character: ${character.name}`;
-  race.textContent = `Race: ${character.species}`;
-  episode.textContent = `episode:`;
-  episode.href = character.episode[0];
-  charDiv.innerHTML = "";
+  const imageRandom = Math.floor(Math.random() * data.results.length); // zufällige auswahl der char. auf der api seite
+  const character = data.results[imageRandom]; // der zufällig gewählte character
+  console.log(character); // nur uim zu sehen was wir für ein Objekt bekommen
+  const img = document.createElement("img"); // erstellen des Img-tags
+  const name = document.createElement("p"); // erstellen  des p-tags für den namen
+  const race = document.createElement("p"); // erstellen  des p-tags für den spezies
+  const episode = document.createElement("a"); // erstellen  des p-tags für den episoden-link
+  img.src = character.image; // einfügen des image links um auf html seite angezeigt zu werden
+  name.textContent = `Character: ${character.name}`; // einfügen des texts um auf html seite angezeigt zu werden
+  race.textContent = `Race: ${character.species}`; // einfügen des texts um auf html seite angezeigt zu werden
+  episode.textContent = `episode`; // einfügen des texts um auf html seite angezeigt zu werden
+  episode.href = character.episode[0]; // href mit link zum episoden guide
+  charDiv.innerHTML = ""; // hier wird das div wieder geleert / damit vermeiden wir das immer neue divs erzeugt werden
+
+  // mit appendChild werden die elemente im html eingefügt
   charDiv.appendChild(img);
   charDiv.appendChild(name);
   charDiv.appendChild(race);
