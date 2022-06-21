d20d6e9f0823218c093e6ab762c2539b6bf8c62e
Holger <holgeraliusklose@gmail.com>
2022-06-21 11:13:20 +0200

    add episoden num && random character all

--- script.js
+++ script.js
@@ -2,41 +2,39 @@
 const charDiv = document.querySelector(".charDiv");
 const button = document.querySelector("#newChar");
 
-const randNum = () => Math.floor(Math.random() * 42) + 1;
-
-const apiCharacterURl = `https://rickandmortyapi.com/api/character/?page=${randNum()}`;
+const apiCharacterURl = `https://rickandmortyapi.com/api/character`;
 
 async function rickMortyCharacters() {
-  const response = await fetch(apiCharacterURl);
+  const randNum = Math.floor(Math.random() * 826);
+  const response = await fetch(`${apiCharacterURl}/${randNum}`);
   const data = await response.json();
   randomCharacters(data);
 }
 
 function randomCharacters(data) {
-  const imageRandom = Math.floor(Math.random() * data.results.length); // zufällige auswahl der char. auf der api seite
-  const character = data.results[imageRandom]; // der zufällig gewählte character
-  console.log(character); // nur uim zu sehen was wir für ein Objekt bekommen
+  //const imageRandom = Math.floor(Math.random() * data.results.length); // zufällige auswahl der char. auf der api seite
+  const character = data; //[imageRandom]; // der zufällig gewählte character
+  console.log(character, data); // nur uim zu sehen was wir für ein Objekt bekommen
   const img = document.createElement("img"); // erstellen des Img-tags
   const name = document.createElement("p"); // erstellen  des p-tags für den namen
   const species = document.createElement("p"); // erstellen  des p-tags für den spezies
   const episode = document.createElement("a"); // erstellen  des p-tags für den episoden-link
-
-  const episodeNum = character.episode.toString().substring(40, 42);
+  const episodeToArr = character.episode[0].split("/");
+  console.log(episodeToArr);
+  const episodeNum = episodeToArr[episodeToArr.length - 1];
+  console.log(episodeNum);
 
   img.src = character.image; // einfügen des image links um auf html seite angezeigt zu werden
   name.textContent = `Character: ${character.name}`; // einfügen des texts um auf html seite angezeigt zu werden
   species.textContent = `Species: ${character.species}`; // einfügen des texts um auf html seite angezeigt zu werden
   episode.textContent = `Episode: ${episodeNum}`; // einfügen des texts um auf html seite angezeigt zu werden
   episode.href = character.episode[0]; // href mit link zum episoden guide
-  charDiv.innerHTML = ""; // hier wird das div wieder geleert / damit vermeiden wir das immer neue divs erzeugt werden
-
-  // mit appendChild werden die elemente im html eingefügt
+  charDiv.innerHTML = ""; // hier wird das div wieder geleert / damit vermeiden wir das immer neue divs erzeugt werde    // mit appendChild werden die elemente im html eingefügt
   charDiv.appendChild(img);
   charDiv.appendChild(name);
   charDiv.appendChild(species);
   charDiv.appendChild(episode);
 }
 
-button.addEventListener("click", rickMortyCharacters, randNum);
+button.addEventListener("click", rickMortyCharacters);
 rickMortyCharacters();
-randNum();
